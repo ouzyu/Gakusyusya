28fbef6d7cbdcda276dcf972ff5c28d9bae2dc12
ouzyu <129580397+ouzyu@users.noreply.github.com>
2023-07-05 11:04:39 +0000

    [save]

--- app/controllers/admin/maps_controller.rb
+++ app/controllers/admin/maps_controller.rb
@@ -2,8 +2,27 @@ class Admin::MapsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
+    
   end
-
+  
+  def create
+  
+  end
+  
   def edit
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+  
+  private
+  
+  def map_params
+    params.require(:map).permit(:name, :image)
   end
 end
