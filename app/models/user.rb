ab62321fcc257210322db7afc716faea1b676fd6
ouzyu <129580397+ouzyu@users.noreply.github.com>
2023-07-07 07:32:43 +0000

    [Update]users/mypageの更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
     h = st / 3600
     m = st % 3600 / 60
     s = st % 60
-    h.to_s + "時間" + m.to_s + "分" + s.to_s + "秒"
+    h.to_s + "時間 " + m.to_s + "分 " + s.to_s + "秒"
   end
 
 end
