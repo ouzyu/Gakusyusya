1bc8952fdfa54fd4aedfd03b8bef50647823e7df
ouzyu <129580397+ouzyu@users.noreply.github.com>
2023-07-12 04:41:06 +0000

    [Add]Animation,Mapモデルにバリデーションを追加

--- app/models/animation.rb
+++ app/models/animation.rb
@@ -3,4 +3,5 @@ class Animation < ApplicationRecord
 
   has_one_attached :image
 
+  validates :image, presence: true, blob: { content_type: :image }
 end
--- app/models/map.rb
+++ app/models/map.rb
@@ -3,4 +3,7 @@ class Map < ApplicationRecord
   has_one_attached :image
 
   has_many :actors, dependent: :destroy
+
+  validates :name,  presence: true
+  validates :image, presence: true, blob: { content_type: :image }
 end
