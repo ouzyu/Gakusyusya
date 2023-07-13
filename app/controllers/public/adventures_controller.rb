f03bf42c21cdcb24c8c9a53c8ddd92781ede689a
ouzyu <129580397+ouzyu@users.noreply.github.com>
2023-07-13 14:06:16 +0000

    [save]

--- app/controllers/public/adventures_controller.rb
+++ app/controllers/public/adventures_controller.rb
@@ -3,7 +3,6 @@ class Public::AdventuresController < ApplicationController
 
   def start
     @quest = Quest.find(params[:quest_id])
-    @quest_time_minutes = @quest.quest_time_to_minutes
     @is_stoped = false
     @stop_time = 0
     incorrect_quest_path
@@ -37,27 +36,27 @@ class Public::AdventuresController < ApplicationController
 
   def unpause
     @quest = Quest.find(params[:quest_id])
-    @stop_time = (Time.now - @quest.pause_time).to_i + @stop_time
+    @stop_time = (Time.now - @quest.pause_time).to_i
     @quest.update(start_time: Time.now)
     @is_stoped = false
+  end
+
+  def finish
+    quest = Quest.find(params[:quest_id])
+    # user = User.find(quest.user_id)
+    # ability = Ability.find(quest.ability_id)
+
+    # user.increment!(level: 1)
+    # user.increment!(study_time: study_time)
+    # ability.increment!(level: 1)
+    # quest.update(is_finished: true)
+
+    redirect_to adventures_boss_path(quest_id: quest.id)
   end
 
   def boss
     @quest = Quest.find(params[:quest_id])
     incorrect_quest_path
-  end
-
-  def finish
-    quest = Quest.find(params[:quest_id])
-    user = User.find(quest.user_id)
-    ability = Ability.find(quest.ability_id)
-
-    user.increment!(level: 1)
-    user.increment!(study_time: study_time)
-    ability.increment!(level: 1)
-    quest.update(is_finished: true)
-
-    redirect_to new_quest_path
   end
 
   private
